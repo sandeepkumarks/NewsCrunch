e327e8028ded9c1755cda3187dbc6f716e83ab69
sandeepkumar.ks <sandeepkumar.ks@zohocorp.com>
2021-12-02 23:52:16 +0530

    Added config file

---
+++ utils/config.js
@@ -0,0 +1,13 @@
+let config = {};
+
+config.firebase = {
+  apiKey: "AIzaSyALzxm-y5LKpYvUoSiHirumKxsDMp4sIT8",
+  authDomain: "news-scraper-2421c.firebaseapp.com",
+  databaseURL: "https://news-scraper-2421c.firebaseio.com",
+  projectId: "news-scraper-2421c",
+  storageBucket: "news-scraper-2421c.appspot.com",
+  messagingSenderId: "34379054726",
+  appId: "1:34379054726:web:a49b4ac00b4096d7"
+};
+
+module.exports = config;
